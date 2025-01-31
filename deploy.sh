f5dad87da50377036a7fba17c1c33a8c53b54df0
vallelym <michael.vallely@bt.com>
2025-01-31 16:11:19 +0000

    Update deploy.sh

--- deploy.sh
+++ deploy.sh
@@ -10,14 +10,14 @@ docker network create my_network
 docker volume create my_volume
 
 # Build the Flask and MySQL images
-docker build -t my_flask_app //task2-pipeline/flask-app
-docker build -t my_mysql_db //task2-pipeline/db
+docker build -t vallelym/task2-pipeline:flask-app ./flask-app
+docker build -t vallelym/task2-pipeline:db ./db
 
 # Run MySQL container
-docker run --name my-mysql --network my_network -e MYSQL_ROOT_PASSWORD=my-secret-pw -v my_volume:/var/lib/mysql -d my_mysql_db
+docker run --name my-mysql --network my_network -e MYSQL_ROOT_PASSWORD=my-secret-pw -v my_volume:/var/lib/mysql -d vallelym/task2-pipeline:db
 
 # Run Flask container
-docker run --name my-flask-app --network my_network -p 5000:5000 -d my_flask_app
+docker run --name my-flask-app --network my_network -p 5000:5000 -d vallelym/task2-pipeline:flask-app
 
 # Run Nginx container
 docker run --name my-nginx --network my_network -p 80:80 -d nginx
